54405044efebc14ea257205a6f8e089f2b94673a
JKolomanski <kubula@onet.eu>
2025-04-16 20:54:50 +0200

    Add time measuring ni players.py

--- Versions/9. Tkinter-edition/players.py
+++ Versions/9. Tkinter-edition/players.py
@@ -1,5 +1,7 @@
 from random import choice
 from MiniMaxNode import MiniMaxNode
+import time
+
 
 
 class Player:
@@ -50,8 +52,14 @@ class MiniMaxPlayer(Player):
         """Handles the turn for a minimax player"""
         enemy_symbol = self.get_enemy_symbol()
         root_node = MiniMaxNode(self.parent.board)
+        start_time = time.perf_counter()
         root_node.expand((self.board_symbol, enemy_symbol))
+        end_time = time.perf_counter()
+        print(f'time to expand: {end_time - start_time:3f} seconds')
+        start_time = time.perf_counter()
         root_node.evaluate_minimax_score((max, min), enemy_symbol, self.board_symbol)
+        end_time = time.perf_counter()
+        print(f'time to evaluate: {end_time - start_time:3f} seconds')
         all_best_nodes = [node for node in root_node.children if node.score == max(root_node.children).score]
         chosen_node = choice(all_best_nodes)
 
